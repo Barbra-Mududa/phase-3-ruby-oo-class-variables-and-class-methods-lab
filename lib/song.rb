53ae442d69bac841526d4f70ee08d4507f899aae
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-10-25 14:39:47 +0300

    Done using tally

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,9 @@ class Song
     @@count = 0
     @@genres = []
     @@artists = []
-
+    @@artist_count = {}
+    @@genre_count = {}
+    
     def initialize(name,artist,genre) 
         @name = name
         @artist = artist
@@ -22,15 +24,7 @@ class Song
     end
 
     def self.genre_count
-        genre_count = {}
-        @@genres.each do |genre|
-            if genre_count[genre]
-                genre_count[genre] += 1 
-            else
-                genre_count[genre] = 1
-            end
-        end
-        genre_count
+        @@genre_count = @@genres.tally
     end
 
     def self.artists
@@ -38,15 +32,7 @@ class Song
     end
     
     def self.artist_count
-        artist_count = {}
-        @@artists.each do |artist|
-            if artist_count[artist]
-                artist_count[artist] += 1 
-            else
-                artist_count[artist] = 1
-            end
-        end
-        artist_count
+        @@artist_count = @@artists.tally
     end
     
     
